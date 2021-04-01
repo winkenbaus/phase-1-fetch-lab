a6f71371dc5f47155c2661f7badc9083b09ba845
caitlin <caitlin.wink@gmail.com>
2021-03-31 19:32:27 -0700

    commit tree 25d63dfa8ad6f760079daa4a6176b8b47001478c

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
-// function fetchBooks() {
-// fetch('https://anapioficeandfire.com/api/books')
-//   .then(function(resp) {
-//     console.log(resp)
-//   return resp.json();
-//   })
-//   .then(function(json) {
-//     console.log(json)
-//   })
-
-
-// function renderBooks(books) {
-//   const main = document.querySelector('main');
-//   books.forEach(book => {
-//     const h2 = document.createElement('h2');
-//     h2.innerHTML = book.name;
-//     main.appendChild(h2);
-//   });
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//    return fetchBooks()
-// });
-// }
-
-
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-    .then(function(resp) {
-      console.log(resp)
-    return resp.json();
-    })
-      .then(function(json) {
-      console.log(json)
-      });
-  }
-  function renderBooks(books) {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+  };
+function renderBooks(book) {
     const main = document.querySelector('main');
-    console.log(books);
-    books.forEach(book => {
+    book.forEach(book => {
       const h2 = document.createElement('h2');
       h2.innerHTML = book.name;
       main.appendChild(h2);
     });
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-     return renderBooks(fetchBooks)
+  };
+document.addEventListener('DOMContentLoaded', function() {
+     return fetchBooks
   });
